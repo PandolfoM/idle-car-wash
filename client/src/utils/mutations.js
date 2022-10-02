64b0997a12167e4d13c65c189b6da4497b3a26ae
Matthew Pandolfo <matt@pandolfo.com>
2022-10-02 18:03:25 -0400

    add products to client mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -78,3 +78,99 @@ export const UPDATE_FOAM = gql`
     }
   }
 `;
+
+export const UPDATE_MITT = gql`
+  mutation Mutation($lvl: Int, $cost: Float, $profit: Float, $speed: Float) {
+    updateMitt(lvl: $lvl, cost: $cost, profit: $profit, speed: $speed) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_SPRAY = gql`
+  mutation Mutation($lvl: Int, $cost: Float, $profit: Float, $speed: Float) {
+    updateSpray(lvl: $lvl, cost: $cost, profit: $profit, speed: $speed) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_DRY = gql`
+  mutation Mutation($lvl: Int, $cost: Float, $profit: Float, $speed: Float) {
+    updateDry(lvl: $lvl, cost: $cost, profit: $profit, speed: $speed) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_VAC = gql`
+  mutation Mutation($lvl: Int, $cost: Float, $profit: Float, $speed: Float) {
+    updateVac(lvl: $lvl, cost: $cost, profit: $profit, speed: $speed) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_CARPET = gql`
+  mutation Mutation($lvl: Int, $cost: Float, $profit: Float, $speed: Float) {
+    updateCarpet(lvl: $lvl, cost: $cost, profit: $profit, speed: $speed) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_SPOT = gql`
+  mutation Mutation($lvl: Int, $cost: Float, $profit: Float, $speed: Float) {
+    updateSpot(lvl: $lvl, cost: $cost, profit: $profit, speed: $speed) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_STEAMER = gql`
+  mutation Mutation($lvl: Int, $cost: Float, $profit: Float, $speed: Float) {
+    updateSteamer(lvl: $lvl, cost: $cost, profit: $profit, speed: $speed) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_CLAY = gql`
+  mutation Mutation($lvl: Int, $cost: Float, $profit: Float, $speed: Float) {
+    updateClay(lvl: $lvl, cost: $cost, profit: $profit, speed: $speed) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_SEALANT = gql`
+  mutation Mutation($lvl: Int, $cost: Float, $profit: Float, $speed: Float) {
+    updateSealant(lvl: $lvl, cost: $cost, profit: $profit, speed: $speed) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_WINDOW = gql`
+  mutation Mutation($lvl: Int, $cost: Float, $profit: Float, $speed: Float) {
+    updateWindow(lvl: $lvl, cost: $cost, profit: $profit, speed: $speed) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_WAFFLE = gql`
+  mutation Mutation($lvl: Int, $cost: Float, $profit: Float, $speed: Float) {
+    updateWaffle(lvl: $lvl, cost: $cost, profit: $profit, speed: $speed) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_SHINE = gql`
+  mutation Mutation($lvl: Int, $cost: Float, $profit: Float, $speed: Float) {
+    updateShine(lvl: $lvl, cost: $cost, profit: $profit, speed: $speed) {
+      _id
+    }
+  }
+`;
