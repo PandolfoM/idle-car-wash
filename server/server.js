595c7feb214892d78b8d4d3617270e888fe17297
Matthew Pandolfo <matt@pandolfo.com>
2022-10-17 10:10:31 -0400

    change server startup

--- server/server.js
+++ server/server.js
@@ -2,18 +2,15 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 const http = require("http");
-require('./config/connection')
+const db = require("./config/connection");
 
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 
-startApolloServer(typeDefs, resolvers);
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-async function startApolloServer(typeDefs, resolvers) {
-  const PORT = process.env.PORT || 3001;
-  const app = express();
-  const httpServer = http.createServer(app);
-
+const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -22,17 +19,26 @@ async function startApolloServer(typeDefs, resolvers) {
 
   await server.start();
   server.applyMiddleware({ app });
-
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/build")));
-  }
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-
-  await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
   console.log(
     `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
   );
+};
+
+startServer()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once("open", () => {
+  app.listen(PORT, () =>
+    console.log(`🌍 Now listening on http://localhost:${PORT}`)
+  );
+});
